d0162b027b0e4ac3d77d9e6f914b02ef4d7ba1b2
Yandri-99 <lumiquinga724@gmail.com>
2026-07-07 18:55:57 -0500

    Shopapp JWT  07-07-2026

--- modulo3/flutter/flutter_shop_app/lib/data/local/secure_storage.dart
+++ modulo3/flutter/flutter_shop_app/lib/data/local/secure_storage.dart
@@ -1,1 +1,32 @@
-// TODO: implement
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  static const _accessKey = 'access_token';
+  static const _refreshKey = 'refresh_token';
+
+  Future<String?> getAccess() => _storage.read(key: _accessKey);
+  Future<String?> getRefresh() => _storage.read(key: _refreshKey);
+
+  Future<void> saveAccessToken(String token) =>
+      _storage.write(key: _accessKey, value: token);
+
+  Future<void> saveRefreshToken(String token) =>
+      _storage.write(key: _refreshKey, value: token);
+
+  Future<void> saveTokens(String access, String refresh) async {
+    await _storage.write(key: _accessKey, value: access);
+    await _storage.write(key: _refreshKey, value: refresh);
+  }
+
+  Future<void> clearSession() async {
+    await _storage.delete(key: _accessKey);
+    await _storage.delete(key: _refreshKey);
+  }
+}
+
+final secureStorageProvider = Provider<SecureStorage>((ref) {
+  return SecureStorage();
+});
